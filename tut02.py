03d314ed5fbd71046e9056aefd09fb88b4a7de9d
Binyam Cheru <binyamcheru123@gmail.com>
2025-06-13 09:06:36 +0300

    simple calc to practice input,type cast and input

---
+++ tut02.py
@@ -0,0 +1,15 @@
+# variables in python
+
+## Simple calculator to practice variables, typecast and input
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+sum = num1 + num2
+difference = num1 - num2
+product = num1*num2
+quotient = num1/num2
+
+print("Sum: ", sum)
+print("Difference: ", difference)
+print("Product: ", product)
+print("Quotient: ", quotient)
