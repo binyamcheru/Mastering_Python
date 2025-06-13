75b30865759d4005801656fcfaf9f3295a28ef1e
Binyam Cheru <binyamcheru123@gmail.com>
2025-06-13 08:47:33 +0300

    002 Python Basics- Syntax and Semantics

---
+++ tut01.py
@@ -0,0 +1,26 @@
+## Single line comment 
+'''
+multiple line comment in python
+another line comment 
+
+'''
+
+print("Hello world!")
+
+## Line continuation 
+## Use backslash(\) to continue a statement to the next line 
+
+total = 1+2+3+4+5+6+7+\
+8+9+9
+print(total)
+
+## Multiple Statements on a single line
+x=5; y=6; z= x+y
+
+print(x,y,z)
+
+# variables in python
+variable = 34
+print(type(variable)) # <class 'int'>
+variable = "Something"
+print(type(variable)) # <class 'int'>
