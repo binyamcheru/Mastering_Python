7a6952022ad7c1fee62af7cbf1428acd474a65cd
Binyam Cheru <binyamcheru123@gmail.com>
2025-06-13 10:07:55 +0300

    operators in python

---
+++ tut03.py
@@ -0,0 +1,25 @@
+## operators in py
+a=10
+b=3
+floor_result = a//b # floor division
+exponent_result = a**b
+
+print(floor_result) # 3.333 becomes 3
+print(exponent_result)
+
+#Comparison operators
+# ==,!= 
+
+# Logical Operators
+# and , or, not
+
+x= True; y=False
+
+result1 = x and y
+print(result1)
+
+result2 = x or y
+print(result2)
+
+z= True
+print(not z)
