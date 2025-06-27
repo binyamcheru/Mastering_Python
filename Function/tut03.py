7f6387e8a0d71f7409c615b004ee43a4acff3346
Binyam Cheru <binyamcheru123@gmail.com>
2025-06-27 07:52:37 +0300

    lambda function

---
+++ Function/tut03.py
@@ -0,0 +1,11 @@
+# Syntax
+# lambda arguments: expression
+
+# def addition(a,b):
+#     return a+b
+
+addition=lambda a,b: a+b
+print(addition(5,8))
+
+is_even = lambda num: num%2==0
+print(is_even(34)) 
